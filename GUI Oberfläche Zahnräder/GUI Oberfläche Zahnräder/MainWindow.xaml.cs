2788e40795e4a45e3d933f27ae87ac01e68b3cb5
la2697 <56022392+la2697@users.noreply.github.com>
2020-12-15 22:03:49 +0100

    Änderung

--- GUI Oberfläche Zahnräder/GUI Oberfläche Zahnräder/MainWindow.xaml.cs
+++ GUI Oberfläche Zahnräder/GUI Oberfläche Zahnräder/MainWindow.xaml.cs
@@ -708,19 +708,18 @@ namespace GUI_Oberfläche_Zahnräder
 
                         // Öffne ein neues Part
                         cc.ErzeugePart();
-                        Console.WriteLine("1");
+                      
 
                         // Erstelle eine Skizze
                         cc.ErstelleLeereSkizze();
-                        Console.WriteLine("2");
+                        
 
                         // Generiere ein Profil
-                        cc.ErzeugeProfil(20, 10);
-                        Console.WriteLine("3");
-
-                        // Extrudiere Balken
-                        cc.ErzeugeBalken(300);
-                        Console.WriteLine("4");
+                        cc.Stirnzahnrad(av);
+                        
+                        
+                       
+                        
                     }
                     else
                     {
